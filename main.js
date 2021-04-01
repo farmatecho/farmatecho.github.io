5b5dc9d7e31cc15844c1143036c6b0921e073412
isajoh0505 <isajoh0505@gmail.com>
2021-04-01 11:17:50 +0200

    Added search function

--- main.js
+++ main.js
@@ -5,10 +5,15 @@ searchBar.addEventListener("keydown", function(e) {
 		search(searchBar.value);
 	}
 });
+var searchButton = document.getElementById("search-button");
+searchButton.addEventListener("click", function(e) {
+	search(searchBar.value);
+});
+
 
 var products = [
 	new product({
-		name: "Antibioticos",
+		name: "Los Antibioticos",
 		imgSrc: "logo.png",
 		price: 50
 	}),
@@ -45,44 +50,78 @@ var products = [
 	})
 ];
 
-function search(str) {
+var productsDisplayed = new Array(products.length);
+for(var i = 0; i < productsDisplayed.length; i++) {
+	productsDisplayed[i] = i;
+}
 
-var p = document.createElement("p");
-	p.innerHTML = "No hay resultados de búsqueda para \"" + str + "\"";
-	document.body.appendChild(p);
+function search(str) {
+	var cleanStr = str.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+	
+	productsDisplayed = [];
+	for(var i = 0; i < products.length; i++) {
+		if(products[i].cleanName.search(cleanStr) != -1) {
+			productsDisplayed.push(i);
+		}
+	}
+	
+	productsDisplayed.sort((a, b) => {
+		return (
+			products[a].cleanName.search(cleanStr) -
+			products[b].cleanName.search(cleanStr)
+		);
+	});
+	
+	updateProducts();
+	
+	var p = document.getElementById("search-error");
+	if(productsDisplayed.length == 0) {
+		p.innerHTML = "No hay resultados de búsqueda para \"" + str + "\"";
+		p.style.display = "block";
+	} else {
+		p.style.display = "none";
+	}
 }
 
 function product(options) {
 	this.name = options.name ?? "Product name";
 	this.imgSrc = options.imgSrc ?? "temp.png";
 	this.price = options.price ?? 1;
+	this.cleanName = this.name.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
 
-for(var i = 0; i < products.length; i++) {
-	var product = products[i];
+updateProducts();
+function updateProducts() {
+	while(productsDiv.lastChild) {
+		productsDiv.removeChild(productsDiv.lastChild);
+	}
 	
-	var productDiv = document.createElement("div");
-	productDiv.className = "product";
-	productsDiv.appendChild(productDiv);
-	
-	var productName = document.createElement("p");
-	productName.innerHTML = product.name;
-	productName.className = "name";
-	productDiv.appendChild(productName);
-	
-	var buyButton = document.createElement("button");
-	buyButton.innerHTML = "Compra";
-	buyButton.addEventListener("click", function() {
-		productsDiv.style.display = "none";
-	});
-	productDiv.appendChild(buyButton);
-	
-	var price = document.createElement("p");
-	price.className = "price";
-	price.innerHTML = product.price + "€";
-	productDiv.appendChild(price);
-	
-	var image = document.createElement("img");
-	image.src = product.imgSrc;
-	productDiv.appendChild(image);
+	for(var i = 0; i < productsDisplayed.length; i++) {
+		var product = products[productsDisplayed[i]];
+
+		var productDiv = document.createElement("div");
+		productDiv.className = "product";
+		productsDiv.appendChild(productDiv);
+
+		var productName = document.createElement("p");
+		productName.innerHTML = product.name;
+		productName.className = "name";
+		productDiv.appendChild(productName);
+
+		var buyButton = document.createElement("button");
+		buyButton.innerHTML = "Compra";
+		buyButton.addEventListener("click", function() {
+			productsDiv.style.display = "none";
+		});
+		productDiv.appendChild(buyButton);
+
+		var price = document.createElement("p");
+		price.className = "price";
+		price.innerHTML = product.price + "€";
+		productDiv.appendChild(price);
+
+		var image = document.createElement("img");
+		image.src = product.imgSrc;
+		productDiv.appendChild(image);
+	}
 }
